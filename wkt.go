e82ea3b0a18a33ba1814968d76759b5c057acfc2
Fabrice <fabrice.vaillant@centraliens.net>
2018-11-11 13:32:44 +0100

    Cleans main entry point

--- wkt.go
+++ wkt.go
@@ -4,16 +4,9 @@ import (
 	"strings"
 
 	"github.com/paulmach/orb"
-	"github.com/pkg/errors"
 )
 
 func Scan(s string) (orb.Geometry, error) {
 	p := Parser{NewLexer(strings.NewReader(s))}
-
-	geo, err := p.Parse()
-	if err != nil {
-		return nil, errors.Wrap(err, "p.Parse error")
-	}
-
-	return geo, nil
+	return p.Parse()
 }
